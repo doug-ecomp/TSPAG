3f2ebd80067f1355068564c4849c62ca6d25f7a3
Rejanio <rejaniomoraes>
2016-06-16 11:30:24 -0300

    Modificado o arquivo burma14.tsp para o formato de leitura da classe Leitura.java. Explico melhor o porque tá dessa forma.

--- src/ag/Leitura.java
+++ src/ag/Leitura.java
@@ -12,21 +12,44 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 /**
  *
  * @author Rejanio
  */
 public class Leitura  {
-    String nome = "../Tsp/src/tsp/burma14.tsp";
-      FileReader arq;
-      BufferedReader lerArq = new BufferedReader(arq);
-
+     
+      private  FileReader fileR;
     public Leitura() throws FileNotFoundException, IOException {
-        this.arq = new FileReader(nome);
+       
+        fileR = new FileReader("../Tsp/src/tsp/burma14.tsp");
+        
     }
-  String linha = lerArq.readLine(); // lê a primeira linha
-
- //falta colocar o restante do código
+/**
+ * Função que ler o arquivo no caminho indicado no construtor
+ * @return retorna o vetor com as coordenadas de cada cidade, o detalhe é que a primeira coluna deve ser composta pelo numero da cidade
+ * 
+ */
+    public float[][] lendo(){
+        String[] numeros = null;
+        ArrayList arrayTemp;
+        Scanner reader = new Scanner(fileR).useDelimiter("#");
+        arrayTemp = new ArrayList();
+        
+        while(reader.hasNext()){
+             arrayTemp.add(reader.next());   
+    }
+        float [][] vetor = new float [14][2];
+        int t=0;
+        for(int x=0;x<14;x++){
+            for(int y=0;y<2;y++){
+                vetor[x][y]=Float.valueOf((String)arrayTemp.get(t)).floatValue();
+                t++;
+                System.out.printf(vetor[x][y]+"\n");
+            }
+        }
+       return vetor;
+    }
 
 }
